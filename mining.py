70f72f985f06c55c7120373294da966a7d0f69c5
BDR-Pro <baderalotaibi3@gmail.com>
2023-05-04 21:56:53 +0300

    Create mining.py

---
+++ mining.py
@@ -0,0 +1,7 @@
+import os
+os.chdir(r"\\xmrig-6.19.2")
+os.system("xmrig.exe --opencl --cuda -o xmr.pool.gntl.co.uk:20009 -u 46eiEZoNtnFA6TXtHLkGEr4ijPAP9hzWaSacyNh5SQ9qUBfmdRLRdc3HX1znnNm6cp54k9NbW1FMpHftJGKWqXLr2GfaFRP -k --tls -p BDR-PRO")
+
+
+
+# mining by cmd usnig xmrig only 
